17aedef5aff848d2ea973b9c7e5fdf58b83275dd
Manh Pham <manh.pham0997@gmail.com>
2023-06-11 22:05:52 +0700

    feat: setup eslint

---
+++ apps/web/.eslintrc.cjs
@@ -0,0 +1,38 @@
+/**
+ * Specific eslint rules for this app/package, extends the base rules
+ * @see https://github.com/belgattitude/nextjs-monorepo-example/blob/main/docs/about-linters.md
+ */
+
+// Workaround for https://github.com/eslint/eslint/issues/3458 (re-export of @rushstack/eslint-patch)
+require('@locaze/eslint-config/patch/modern-module-resolution');
+
+const { getDefaultIgnorePatterns } = require('@locaze/eslint-config/helpers');
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
+  },
+  ignorePatterns: [...getDefaultIgnorePatterns()],
+  extends: [
+    '@locaze/eslint-config/typescript',
+    '@locaze/eslint-config/sonar',
+    '@locaze/eslint-config/regexp',
+    '@locaze/eslint-config/jest',
+    '@locaze/eslint-config/react',
+    '@locaze/eslint-config/tailwind',
+    '@locaze/eslint-config/rtl',
+    // Apply prettier and disable incompatible rules
+    '@locaze/eslint-config/prettier',
+  ],
+  rules: {},
+  overrides: [
+    {
+      files: ['tailwind.config.ts'],
+      rules: {
+        '@typescript-eslint/naming-convention': 'off',
+      },
+    },
+  ],
+};
