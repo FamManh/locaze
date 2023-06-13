ec81a95fc92225c90b6f9409d69ffd57033bb4ec
Manh Pham <manh@Manh-M.local>
2023-06-13 23:34:51 +0700

    setup typeorm

---
+++ apps/server/ormconfig.ts
@@ -0,0 +1,18 @@
+import 'dotenv';
+import { DataSource } from 'typeorm';
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  //   namingStrategy: new SnakeNamingStrategy(),
+  //   subscribers: [UserSubscriber],
+  entities: [
+    'src/modules/**/*.entity{.ts,.js}',
+    'src/modules/**/*.view-entity{.ts,.js}',
+  ],
+  migrations: ['src/database/migrations/*{.ts,.js}'],
+});
