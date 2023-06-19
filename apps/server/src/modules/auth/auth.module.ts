cf90bb537ba5478f90b2e1a87e4bee7b62c1e117
Manh Pham <manh.pham0997@gmail.com>
2023-06-19 13:03:26 +0700

    feat: implement database, app, auth config

--- apps/server/src/modules/auth/auth.module.ts
+++ apps/server/src/modules/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
@@ -7,11 +8,15 @@ import { AuthService } from './auth.service';
 @Module({
   imports: [
     UserModule,
-    JwtModule.register({
-      global: true,
-      // todo: get from constant
-      secret: '123123',
-      signOptions: { expiresIn: '60s' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('auth.secret'),
+        signOptions: {
+          expiresIn: configService.get('auth.expires'),
+        },
+      }),
     }),
   ],
   controllers: [AuthController],
